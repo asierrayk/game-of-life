c1b9788b7d609fbbad2bab4d9a033f25201ee341
asierrayk <asier314@gmail.com>
2017-06-08 23:59:31 +0200

    small refactorization

---
+++ game-of-life/grid.js
@@ -0,0 +1,59 @@
+function Grid(){
+	this.grid = new Array(GRID_SIZE+2);
+	this.nextGrid = new Array(GRID_SIZE+2);
+	this.neighbors = new Array(GRID_SIZE+2);
+	
+	
+	for (var i = 0; i < GRID_SIZE+2; i++) {
+	  this.grid[i] = new Array(GRID_SIZE+2);
+	  this.nextGrid[i] = new Array(GRID_SIZE+2);
+	  this.neighbors[i] = new Array(GRID_SIZE+2);
+	  
+	  for (var j = 0; j < GRID_SIZE+2; j++){
+		  this.grid[i][j] = 0;
+		  this.nextGrid[i][j] = 0;
+		  this.neighbors[i][j] = 0;
+	  }
+	}
+	
+	this.grid[1][1] = 1;
+	this.grid[2][2] = 1;
+	this.grid[3][3] = 1;
+	this.grid[3][4] = 1;
+	this.grid[4][3] = 1;
+	
+	this.show = function(){
+		for (var i = 1; i <= GRID_SIZE; i++) {
+			for (var j = 1; j <= GRID_SIZE; j++) {
+				if (this.grid[i][j] == 1) {
+					fill(255,255,255);
+				}
+				else {
+					fill(0,0,0);
+				}
+				rect((i-1)*CELL_SIZE, (j-1)*CELL_SIZE, CELL_SIZE, CELL_SIZE);
+			}
+		}
+	}
+	
+	this.update = function(){
+		for (var i = 1; i <= GRID_SIZE; i++) {
+			for (var j = 1; j <= GRID_SIZE; j++) {
+				
+				var n = this.grid[i-1][j-1] + this.grid[i][j-1] + this.grid[i+1][j-1] + this.grid[i-1][j] + this.grid[i+1][j] + this.grid[i-1][j+1] + this.grid[i][j+1] + this.grid[i+1][j+1]; 
+				
+				this.nextGrid[i][j] = 0;
+				if (this.grid[i][j] == 1 && n == 2 || n == 3) this.nextGrid[i][j] = 1;
+				if (this.grid[i][j] == 0 && n == 3) this.nextGrid[i][j] = 1;
+
+			}
+		}
+
+
+		for (var i=0; i < GRID_SIZE+2; i++){
+			for (var j=0; j < GRID_SIZE+2; j++){
+				this.grid[i][j] = this.nextGrid[i][j];
+			}
+		}
+	}
+}
--- game-of-life/sketch.js
+++ game-of-life/sketch.js
@@ -5,26 +5,7 @@ var grid;
 var nextGrid;
 
 function setup() {
-	grid = new Array(GRID_SIZE+2);
-	nextGrid = new Array(GRID_SIZE+2);
-	for (var i = 0; i < GRID_SIZE+2; i++) {
-	  grid[i] = new Array(GRID_SIZE+2);
-	  nextGrid[i] = new Array(GRID_SIZE+2);
-	  
-	  for (var j = 0; j < GRID_SIZE+2; j++){
-		  grid[i][j] = 0;
-		  nextGrid[i][j] = 0;
-	  }
-	}
-	
-	
-	grid[2][1] = 1;
-	grid[3][2] = 1;
-	grid[3][3] = 1;
-	grid[2][3] = 1;
-	grid[1][3] = 1;
-	
-	
+	grid = new Grid();
 	createCanvas(CANVAS_SIZE, CANVAS_SIZE);
 }
 
@@ -37,37 +18,13 @@ function draw() {
 	stroke(255, 204, 0);
 	
 	//draw
-	for (var i = 1; i <= GRID_SIZE; i++) {
-		for (var j = 1; j <= GRID_SIZE; j++) {
-			if (grid[i][j] == 1) {
-				fill(255,255,255);
-			}
-			else {
-				fill(0,0,0);
-			}
-			rect((i-1)*CELL_SIZE, (j-1)*CELL_SIZE, CELL_SIZE, CELL_SIZE);
-		}
-	}
+	grid.show();
 	
 	
 	
 	//update
-	for (var i = 1; i <= GRID_SIZE; i++) {
-		for (var j = 1; j <= GRID_SIZE; j++) {
-			
-			var neighbors = grid[i-1][j-1] + grid[i][j-1] +grid[i+1][j-1] +grid[i-1][j] +grid[i+1][j] +grid[i-1][j+1] +grid[i][j+1] +grid[i+1][j+1]; 
-			
-			nextGrid[i][j] = 0;
-			if (grid[i][j] == 1 && neighbors == 2 || neighbors == 3) nextGrid[i][j] = 1;
-			if (grid[i][j] == 0 && neighbors == 3) nextGrid[i][j] = 1;
-
-		}
-	}
+	grid.update();
 	
-	for (var i=0; i < GRID_SIZE+2; i++){
-		for (var j=0; j < GRID_SIZE+2; j++){
-			grid[i][j] = nextGrid[i][j];
-		}
-	}
+	
 	}
 }
